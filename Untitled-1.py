f90761a9ae3f9f79da203c747ba00088b85ef3e4
shaimalq <146936338+shaimalq@users.noreply.github.com>
2023-10-18 23:40:02 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,35 @@
+ #Écrire un algorithme qui compte le nombre de caractères dans une chaîne.
+a=input("donner les caracteres :")
+print(len(a))
+
+#Écrire un algorithme qui supprime tous les espaces d'une chaîne.
+x=input("entrer des caracteres:")
+print(x.strip())
+
+#Écrire un algorithme qui convertit une chaîne en majuscules.
+b=input("donner les caracteres:")
+print(b.upper())
+
+#Écrire un algorithme qui compte le nombre d'occurrences d'un caractère dans une chaîne.
+
+f="java,php,mysql,php"
+print(f.count("php"))
+
+#Écrire un algorithme qui remplace toutes les occurrences d'un caractère par un autre dans une chaîne.
+
+g=input("veuillez saisir des caracteres:")
+print(g.replace("","new value"))
+ 
+ #Écrire un algorithme qui trouve la position d'une sous-chaîne dans une chaîne donnée.
+
+p=input("veuillez saisir des caracteres:")
+print(p.center(10))
+
+#Écrire un algorithme qui renverse une chaîne de caractères.
+t=input("veuillez saisir des caracteres:")
+t.reverse()
+print(t)
+
+# Écrire un algorithme qui vérifie si une chaîne est vide ou non.
+k=input("veuillez saisir des caracteres:")
+print(k.count())
